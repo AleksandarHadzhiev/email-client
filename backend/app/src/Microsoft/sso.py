b46e6fe9b98cc9fa1136de75146a74490eac308b
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-01-16 16:31:25 +0100

    fetching microsoft emails

--- backend/app/src/Microsoft/sso.py
+++ backend/app/src/Microsoft/sso.py
@@ -1,4 +1,5 @@
 import identity.web
+import requests
 from app.settings import Settings
 from fastapi import  Request
 from app.src.Microsoft.service import MicrosoftService
@@ -9,6 +10,7 @@ class MicrosoftSSO():
         self.authority =f"https://login.microsoftonline.com/common"
         self.settings = settings
         self.service = MicrosoftService()
+        self.auth: identity.web.Auth = None
 
 
     def set_auth(self, auth):
@@ -27,7 +29,7 @@ class MicrosoftSSO():
             scopes=self.settings.MICROSOFT_SCOPE,
             redirect_uri=self.settings.REDIRECT_URI,
         )
-        redirec_uri = f'{response["auth_uri"]}&login_hint={email}'  
+        redirec_uri = f'{response["auth_uri"]}&login_hint={email}'
         return redirec_uri
 
     async def logout(self):
@@ -35,9 +37,19 @@ class MicrosoftSSO():
 
 
     async def callback(self, request: Request):
-        body = request
+        body = await request.json()
         login_data = self.service.get_data_for_login(request_body=body)
         result = self.auth.complete_log_in(login_data)
+        if "error" in result:
+            raise result["error"]
         user = self.auth.get_user()
         return user["preferred_username"]
-        
+
+
+    async def search_messages(self):
+        token = self.auth.get_token_for_user(self.settings.MICROSOFT_SCOPE)
+        endpoint = "https://graph.microsoft.com/v1.0/me/messages"
+        headers = {"Authorization": f"Bearer {token['access_token']}"}
+        response = requests.get(endpoint,headers=headers)
+        s = response.json()
+        return s
