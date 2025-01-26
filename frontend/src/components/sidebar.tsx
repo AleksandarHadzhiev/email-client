c5c1e94157779c80166b65edd79ca9be3456389f
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-01-26 10:48:59 +0100

    send message button added

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -18,6 +18,20 @@ export default function Sidebar({ setMail }) {
     }, [mails.length])
     return (
         <aside className="col-span-3 bg-gray-50 dark:bg-gray-800 overflow-y-auto">
+            <button type="button" className="flex m-2 bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-solid border-2 border-white hover:border-transparent rounded">
+                Send new message
+                <svg
+                    className="h-8 w-8 ml-2"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <line x1="22" y1="2" x2="11" y2="13" />
+                    <polygon points="22 2 15 22 11 13 2 9 22 2" />
+                </svg>
+            </button>
             <ul className="space-y-2 font-medium">
                 {mails.map((email, index) => (
 
