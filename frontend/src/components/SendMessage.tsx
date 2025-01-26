df8c7b8beebeb7b19ddf74bfe70f265eb8d130e0
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-01-26 11:07:37 +0100

    basic setup for the SendMessage component

--- frontend/src/components/SendMessage.tsx
+++ frontend/src/components/SendMessage.tsx
@@ -1,8 +1,16 @@
 //@ts-ignore
 export default function SendMessage({ setSendMessageIsActivated }) {
     return (
-        <section>
-            <button onClick={() => { setSendMessageIsActivated(false) }}>Close</button>
+        <section
+            className="absolute bottom-0 right-0 bg-white text-black min-h-64 min-w-80 border-2 border-solid">
+            <div
+                className="bg-gray border-b-2 border-indigo-500">
+                <button
+                    onClick={() => { setSendMessageIsActivated(false) }}>
+                    Close
+                </button>
+
+            </div>
             {/* Topbar */}
             {/* Body */}
             {/* Footer */}
