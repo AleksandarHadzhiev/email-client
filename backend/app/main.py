7ce54f8f0186146cb8655215b13b05ccbd731184
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-02-02 14:53:29 +0100

    basic todos init

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from app.settings import Settings
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from app.src.Login.router import LoginRouter
+from app.src.Todos.router import TodoRouter
+from app.db import DBConnector
+from typing import Annotated
+from sqlmodel import Session
 
 
 settings = Settings()
@@ -11,8 +15,14 @@ settings = Settings()
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="session")
 
+db = DBConnector()
+SessionDep = Annotated[Session, Depends(db.get_session())]
+
 login_router = LoginRouter(settings=settings)
+todo_router = TodoRouter(settings=settings, session=SessionDep)
+
 app.include_router(login_router.router)
+app.include_router(todo_router.router)
 origins = [
     "*"
 ]
--- backend/app/src/Todos/router.py
+++ backend/app/src/Todos/router.py
@@ -1,34 +1,40 @@
 import json
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response
 from app.settings import Settings
 from app.src.Todos.service import ToDoService
-
+from sqlmodel import Session
 class TodoRouter():
     
-    def __init__(self, settings:Settings):
+    def __init__(self, settings:Settings, session: Session):
         self.settings = settings
+        self.session = session
         self.router = APIRouter()
         self.service = ToDoService(settings=settings)
-        self.router.add_api_route("/", self.create, methods=["POST"])
-        self.router.add_api_route("/{user_id}", self.get_todos, methods=["GET"])
-        self.router.add_api_route("/{todo_id}", self.edit, methods=["PUT"])
-        self.router.add_api_route("/{todo_id}", self.delete, methods=["DELETE"])
-        
+        self.router.add_api_route("/todo", self.index, methods=["GET"])
+        self.router.add_api_route("/todo", self.create, methods=["POST"])
+        self.router.add_api_route("/todo/{email}", self.get_todos, methods=["GET"])
+        self.router.add_api_route("/todo/{id}", self.edit, methods=["PUT"])
+        self.router.add_api_route("/todo/{id}", self.delete, methods=["DELETE"])
+
+
+    async def index(self):
+        return  '<a class="button" href="/login">ToDo Login</a>'
+
 
     async def create(self, request: Request):
         print(await request)
         pass
 
 
-    async def get_todos(self, user_id: int):
-        pass
+    async def get_todos(self, email: str):
+        return Response(content=json.dumps({"email": email}), media_type="json")
 
 
-    async def edit(self, request: Request, todo_id: int):
+    async def edit(self, request: Request, id: int):
         print(await request)
         pass
 
 
-    async def delete(self, todo_id: int):
+    async def delete(self, id: int):
         pass
